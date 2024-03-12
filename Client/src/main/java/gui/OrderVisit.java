f68a2478975e8a1b5b30e97d64dfd153daf8d567
Nir <nir.froimo@gmail.con>
2024-03-12 14:00:27 +0200

    Tour guide Login(need to fix)

--- Client/src/main/java/gui/OrderVisit.java
+++ Client/src/main/java/gui/OrderVisit.java
@@ -1,6 +1,13 @@
 package gui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -10,4 +17,26 @@ public class OrderVisit implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
 
     }
+
+    @FXML
+    public void OrderVisit(ActionEvent actionEvent) {
+        TextField idField = new TextField();
+        idField.setPromptText("ID");
+
+        TextField fullNameField = new TextField();
+        fullNameField.setPromptText("Full Name");
+
+        Spinner<Integer> visitorCountSpinner = new Spinner<>(1, 100, 1);
+
+        DatePicker datePicker = new DatePicker();
+
+        ChoiceBox<String> timeChoiceBox = new ChoiceBox<>();
+        timeChoiceBox.getItems().addAll("Morning", "Afternoon", "Evening");
+
+        // Add these to your scene or layout
+        // For example, if you're using a VBox layout:
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(idField, fullNameField, visitorCountSpinner, datePicker, timeChoiceBox);
+    }
+
 }
